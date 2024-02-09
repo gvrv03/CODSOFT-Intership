d434895df611cd2f901cf869a67bfcf4ac3fcdd7
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-02-09 15:04:24 +0530

    Remove some unwanted character

--- task1/src/components/Home/Analystics.js
+++ task1/src/components/Home/Analystics.js
@@ -2,56 +2,56 @@ import React from "react";
 
 const Analystics = () => {
   return (
-    <section class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-wrap -m-4 text-center">
-          <div class="p-4 md:w-1/4 sm:w-1/2 w-full bg-white">
-            <div class="border-2 flex gap-5 items-center justify-around  border-gray-200 px-4 py-6 rounded-lg">
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4 text-center">
+          <div className="p-4 md:w-1/4 sm:w-1/2 w-full bg-white">
+            <div className="border-2 flex gap-5 items-center justify-around  border-gray-200 px-4 py-6 rounded-lg">
               <i className="uil uil-users-alt text-5xl" />
               <div>
-                <h2 class="title-font font-medium text-3xl text-gray-900">
+                <h2 className="title-font font-medium text-3xl text-gray-900">
                   2.7K
                 </h2>
-                <p class="leading-relaxed">Users</p>
+                <p className="leading-relaxed">Users</p>
               </div>
             </div>
           </div>
 
-          <div class="p-4 md:w-1/4 sm:w-1/2 w-full bg-white">
-            <div class="border-2 flex gap-5 items-center justify-around  border-gray-200 px-4 py-6 rounded-lg">
+          <div className="p-4 md:w-1/4 sm:w-1/2 w-full bg-white">
+            <div className="border-2 flex gap-5 items-center justify-around  border-gray-200 px-4 py-6 rounded-lg">
               <i className="uil uil-building text-5xl" />
               <div>
-                <h2 class="title-font font-medium text-3xl text-gray-900">
+                <h2 className="title-font font-medium text-3xl text-gray-900">
                   1.5K
                 </h2>
-                <p class="leading-relaxed">Colleges</p>
+                <p className="leading-relaxed">Colleges</p>
               </div>
             </div>
           </div>
 
 
 
-          <div class="p-4 md:w-1/4 sm:w-1/2 w-full bg-white">
-            <div class="border-2 flex gap-5 items-center justify-around  border-gray-200 px-4 py-6 rounded-lg">
+          <div className="p-4 md:w-1/4 sm:w-1/2 w-full bg-white">
+            <div className="border-2 flex gap-5 items-center justify-around  border-gray-200 px-4 py-6 rounded-lg">
               <i className="uil uil-file-check text-5xl" />
               <div>
-                <h2 class="title-font font-medium text-3xl text-gray-900">
+                <h2 className="title-font font-medium text-3xl text-gray-900">
                   3.2K
                 </h2>
-                <p class="leading-relaxed">Registration</p>
+                <p className="leading-relaxed">Registration</p>
               </div>
             </div>
           </div>
 
 
-          <div class="p-4 md:w-1/4 sm:w-1/2 w-full bg-white">
-            <div class="border-2 flex gap-5 items-center justify-around  border-gray-200 px-4 py-6 rounded-lg">
+          <div className="p-4 md:w-1/4 sm:w-1/2 w-full bg-white">
+            <div className="border-2 flex gap-5 items-center justify-around  border-gray-200 px-4 py-6 rounded-lg">
               <i className="uil uil-user-check text-5xl" />
               <div>
-                <h2 class="title-font font-medium text-3xl text-gray-900">
+                <h2 className="title-font font-medium text-3xl text-gray-900">
                   3.2K
                 </h2>
-                <p class="leading-relaxed">Admissions</p>
+                <p className="leading-relaxed">Admissions</p>
               </div>
             </div>
           </div>
--- task3/src/components/MockUp.js
+++ task3/src/components/MockUp.js
@@ -4,12 +4,12 @@ import Calc from "./Calc";
 const MockUp = () => {
   return (
     <>
-      <div class="  relative mx-auto border-black dark:border-black bg-black border-[14px] rounded-[2.5rem] h-[600px] w-[300px]">
-        <div class="h-[32px] w-[3px] bg-black dark:bg-black absolute -start-[17px] top-[72px] rounded-s-lg"></div>
-        <div class="h-[46px] w-[3px] bg-black dark:bg-black absolute -start-[17px] top-[124px] rounded-s-lg"></div>
-        <div class="h-[46px] w-[3px] bg-black dark:bg-black absolute -start-[17px] top-[178px] rounded-s-lg"></div>
-        <div class="h-[64px] w-[3px] bg-black dark:bg-black absolute -end-[17px] top-[142px] rounded-e-lg"></div>
-        <div class="rounded-[2rem]  border border-gray-950 p-5 text-white overflow-hidden w-[272px] h-[572px] bg-white dark:bg-black">
+      <div className="  relative mx-auto border-black dark:border-black bg-black border-[14px] rounded-[2.5rem] h-[600px] w-[300px]">
+        <div className="h-[32px] w-[3px] bg-black dark:bg-black absolute -start-[17px] top-[72px] rounded-s-lg"></div>
+        <div className="h-[46px] w-[3px] bg-black dark:bg-black absolute -start-[17px] top-[124px] rounded-s-lg"></div>
+        <div className="h-[46px] w-[3px] bg-black dark:bg-black absolute -start-[17px] top-[178px] rounded-s-lg"></div>
+        <div className="h-[64px] w-[3px] bg-black dark:bg-black absolute -end-[17px] top-[142px] rounded-e-lg"></div>
+        <div className="rounded-[2rem]  border border-gray-950 p-5 text-white overflow-hidden w-[272px] h-[572px] bg-white dark:bg-black">
           <Calc />
         </div>
       </div>
